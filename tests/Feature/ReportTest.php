f8d37a06cd1dbe98d7246fe1029a69ea9a3e908a
Tony Messias <tonysm@hey.com>
2024-03-23 18:55:24 -0300

    Adds one more test for reports without reference date

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -9,6 +9,8 @@ use Modules\User\Entities\User;
 it('requests report', function () {
     Mail::fake();
 
+    $this->freezeTime();
+
     $user = User::factory()
         ->has(TimeEntry::factory()->times(3)->state([
             'started_at' => now()->subMonth()->startOfMonth()->addDay()->startOfDay(),
@@ -31,6 +33,34 @@ it('requests report', function () {
         $mail->assertSeeInHtml('Total: 3h');
 
         return $mail->reference->is($reference) && $mail->user->is($user);
+   });
+});
+
+it('requests report (for current month - without reference date)', function () {
+    Mail::fake();
+
+    $this->freezeTime();
+
+    $user = User::factory()
+        ->has(TimeEntry::factory()->times(3)->state([
+            'started_at' => now()->startOfDay(),
+            'ended_at' => now()->startOfDay()->addHour(),
+        ]), 'entries')
+        // Shouldn't include this...
+        ->has(TimeEntry::factory([
+            'ended_at' => null,
+        ]), 'entries')
+        ->create();
+
+    Sanctum::actingAs($user, ['*']);
+
+    $this->postJson(route('reports.store'))->assertAccepted();
+
+    Mail::assertQueued(function (MonthReport $mail) use ($user) {
+        $mail->assertSeeInHtml('Relatório do Mês');
+        $mail->assertSeeInHtml('Total: 3h');
+
+        return $mail->reference->is(now()) && $mail->user->is($user);
    });
 });
 
